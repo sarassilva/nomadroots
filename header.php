7d441b3781e480136369ea0ca593fadfdca8b92f
Sara <sarassilvadw@gmail.com>
2023-11-08 14:27:41 -0300

    Update header.php

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 <meta name="description" content="<?php bloginfo('description'); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5">
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.less" />
+<link rel="stylesheet/less" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.less" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php wp_head(); ?>
 </head>
