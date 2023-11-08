9ec21d4730d48e64a3b6e5877a80bed5383f2e0f
Sara <sarassilvadw@gmail.com>
2023-11-08 17:33:14 -0300

    Page sobre

---
+++ templates/sobre.php
@@ -0,0 +1,18 @@
+<?php 
+/* Template Name: Sobre Nós */ 
+?>
+
+<?php get_header(); ?>
+
+<main class="aboutNomad">
+
+	<section class="main-banner" style="background-image: url('<?php the_field('imagem_destacada') ?>')">
+		<div class="content">
+			<img src="<?php the_field('logo') ?>" alt="Nomad Roots" />
+			<h1><?php the_field('titulo_h1') ?></h1>
+		</div>
+	</section>
+	
+</main>
+
+<?php get_footer(); ?>
