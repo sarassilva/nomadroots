65297b0f5a89babc41ed67d4cb26f7354f3a925f
Sara <sarassilvadw@gmail.com>
2023-11-08 18:03:14 -0300

    Updates sobre

--- templates/sobre.php
+++ templates/sobre.php
@@ -19,6 +19,55 @@
 		</div>
 	</section>
 
+	<section class="video">
+		<?php
+			// Load value.
+			$iframe = get_field('video');
+
+			// Use preg_match to find iframe src.
+			preg_match('/src="(.+?)"/', $iframe, $matches);
+			$src = $matches[1];
+
+			// Add extra parameters to src and replace HTML.
+			$params = array(
+			    'controls'  => 0,
+			    'hd'        => 1,
+			    'autohide'  => 1
+			);
+			$new_src = add_query_arg($params, $src);
+			$iframe = str_replace($src, $new_src, $iframe);
+
+			// Add extra attributes to iframe HTML.
+			$attributes = 'frameborder="0"';
+			$iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
+
+			// Display customized HTML.
+			echo $iframe;
+			?>
+	</section>
+
+	<section class="nomadCarousel">
+		<div class="container">
+			<h2><?php the_field('titulo_h2') ?> </h2>
+			<h3><?php the_field('subtitulo') ?> </h3>
+
+			<div class="glider-contain">
+		    	<div class="nCarousel">
+			    	<?php if( have_rows('carousel_nomad') ): ?>
+			    		<?php while( have_rows('carousel_nomad') ): the_row(); ?>
+			    			<div class="content">
+								<h4><?php the_field('titulo') ?></h4>
+								<p><?php the_field('texo') ?></p>
+			    			</div>
+			    		<?php endwhile; ?>
+			    	<?php endif; ?>				    	
+			    </div>
+			</div>
+
+		</div>
+	</section>
+
+
 </main>
 
 <?php get_footer(); ?>
