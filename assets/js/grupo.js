4a743b4e9de50fdb7c303d3d4a3fd846c3075b68
Sara <sarassilvadw@gmail.com>
2023-11-10 17:13:09 -0300

    Update grupo.js

--- assets/js/grupo.js
+++ assets/js/grupo.js
@@ -1,55 +1,32 @@
-window.addEventListener('load', function () {
-    // Obtém todas as divs com a classe '.glider' na página
-    const divs = document.querySelectorAll('.glider');
-
-    // Itera sobre cada div
-    divs.forEach((div, index) => {
-        // Adiciona um número à classe existente ou adiciona uma nova classe com o número
-        const gliderClass = `glider-${index + 1}`;
-        div.classList.add(gliderClass);
-
-        // Adiciona IDs às setas de navegação específicas para este carrossel, se existirem
-        const prevArrow = div.querySelector('.glider-prev');
-        const nextArrow = div.querySelector('.glider-next');
-
-        if (prevArrow) {
-            prevArrow.id = `seta-prev-${index + 1}`;
-        }
-
-        if (nextArrow) {
-            nextArrow.id = `seta-next-${index + 1}`;
-        }
-
-        // Inicializa o Glider.js para cada div
-        const glider = new Glider(div, {
-            slidesToShow: 'auto',
-            slidesToScroll: 1,
-            itemWidth: 800,
-            draggable: false,
-            scrollLock: false,
-            dots: false,
-            rewind: true,
-            arrows: {
-                prev: prevArrow ? `#${prevArrow.id}` : null,
-                next: nextArrow ? `#${nextArrow.id}` : null
-            }
+ window.addEventListener('load', function () {
+            [].forEach.call(document.querySelectorAll('.glider'), function (ele) {
+                ele.addEventListener('glider-slide-visible', function (event) {
+                    var glider
+                        = Glider(this); console.log('Slide Visible %s', event.detail.slide)
+                });
+                ele.addEventListener('glider-slide-hidden', function (event) {
+                    console.log('Slide Hidden %s', event.detail.slide)
+                });
+                ele.addEventListener('glider-refresh', function (event) {
+                    console.log('Refresh')
+                });
+                ele.addEventListener('glider-loaded', function
+                    (event) {
+                        console.log('Loaded')
+                });
+                new Glider(ele, {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    scrollLock: true,
+                    draggable: true,
+                    dots: ele.querySelector('.dots'),
+                    arrows: {
+                        prev: ele.querySelector('.glider-prev'),
+                        next: ele.querySelector('.glider-next')
+                    }
+                });
+            });
         });
-
-        // Adiciona um ouvinte de evento para as setas de navegação, se existirem
-        if (prevArrow) {
-            prevArrow.addEventListener('click', function () {
-                glider.scrollItem('prev', { scrollBy: 1 });
-            });
-        }
-
-        if (nextArrow) {
-            nextArrow.addEventListener('click', function () {
-                glider.scrollItem('next', { scrollBy: 1 });
-            });
-        }
-    });
-});
-
 
 
 
