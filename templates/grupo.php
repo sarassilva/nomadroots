96e5ff7670aa57be02f9751041cbfa92afef91e8
Sara <sarassilvadw@gmail.com>
2023-11-10 16:27:47 -0300

    Update grupo.php

--- templates/grupo.php
+++ templates/grupo.php
@@ -54,6 +54,8 @@
 							    		<?php endwhile; ?>
 							    	<?php endif; ?>				    	
 							    </div>
+							    <button class="glider-prev">&lsaquo;</button>
+						        <button class="glider-next">&rsaquo;</button>
 							</div>
 
 		            	</div>
