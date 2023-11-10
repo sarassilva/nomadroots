65cf74eb5f535747be8b7065da622f3766481d3d
Sara <sarassilvadw@gmail.com>
2023-11-10 16:48:32 -0300

    Update grupo.js

--- assets/js/grupo.js
+++ assets/js/grupo.js
@@ -1,10 +1,4 @@
 window.addEventListener('load', function () {
-    // Adiciona classes às setas de navegação
-    const arrows = document.querySelectorAll('.glider-prev, .glider-next');
-    arrows.forEach((arrow, index) => {
-        arrow.classList.add(`seta-${index + 1}`);
-    });
-
     // Obtém todas as divs com a classe '.glider' na página
     const divs = document.querySelectorAll('.glider');
 
@@ -13,12 +7,8 @@ window.addEventListener('load', function () {
         // Adiciona um número à classe existente ou adiciona uma nova classe com o número
         div.classList.add(`glider-${index + 1}`);
 
-        // Obtém os seletores específicos para as setas de navegação desta div
-        const prevArrowSelector = `.glider-${index + 1} .glider-prev.seta-${index + 1}`;
-        const nextArrowSelector = `.glider-${index + 1} .glider-next.seta-${index + 1}`;
-
         // Inicializa o Glider.js para cada div
-        new Glider(div, {
+        const glider = new Glider(div, {
             slidesToShow: 'auto',
             slidesToScroll: 1,
             itemWidth: 800,
@@ -26,10 +16,24 @@ window.addEventListener('load', function () {
             scrollLock: false,
             dots: false,
             rewind: true,
-            arrows: {
-                prev: prevArrowSelector,
-                next: nextArrowSelector
-            }
         });
+
+        // Obtém as setas de navegação específicas para este carrossel
+        const prevArrow = div.querySelector('.glider-prev');
+        const nextArrow = div.querySelector('.glider-next');
+
+        // Adiciona um ouvinte de evento para as setas de navegação
+        if (prevArrow) {
+            prevArrow.addEventListener('click', function () {
+                glider.scrollItem('prev', { scrollBy: 1 });
+            });
+        }
+
+        if (nextArrow) {
+            nextArrow.addEventListener('click', function () {
+                glider.scrollItem('next', { scrollBy: 1 });
+            });
+        }
     });
 });
+
