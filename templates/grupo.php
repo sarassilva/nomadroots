3f8906d9d6f14427885bbf0e44400e080d0d079c
Sara <sarassilvadw@gmail.com>
2023-11-10 09:34:31 -0300

    Update grupo.php

--- templates/grupo.php
+++ templates/grupo.php
@@ -7,7 +7,7 @@
 <main class="groupNomad">
 	<section class="main-banner" style="background-image: url('<?php the_field('imagem_destacada') ?>')">
 		<div class="content">
-			<img src="<?php the_field('logo') ?>" alt="Nomad Roots" />
+			<div class="groupName"><span>Grupo</span>Nomad</div>
 			<h1><?php the_field('titulo_h1') ?></h1>
 			<a href="<?php the_field('link') ?>" target="_blank" title="<?php the_field('texto_do_botao') ?>"><?php the_field('texto_do_botao') ?></a>
 		</div>
