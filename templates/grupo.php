5f748d66a5982d44a1fd23511d3775b3ed8aaac5
Sara <sarassilvadw@gmail.com>
2023-11-10 09:40:47 -0300

    Update grupo.php

--- templates/grupo.php
+++ templates/grupo.php
@@ -9,7 +9,7 @@
 		<div class="content">
 			<div class="groupName"><span>Grupo</span>Nomad</div>
 			<h1><?php the_field('titulo_h1') ?></h1>
-			<a href="<?php the_field('link') ?>" target="_blank" title="<?php the_field('texto_do_botao') ?>"><?php the_field('texto_do_botao') ?></a>
+			<a href="<?php the_field('link') ?>" class="btn" target="_blank" title="<?php the_field('texto_do_botao') ?>"><?php the_field('texto_do_botao') ?></a>
 		</div>
 	</section>
 
