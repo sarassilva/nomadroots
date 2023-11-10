8a35fb182cacc9089f14884469803f0c3b359ee1
Sara <sarassilvadw@gmail.com>
2023-11-10 16:19:59 -0300

    Update grupo.js

--- assets/js/grupo.js
+++ assets/js/grupo.js
@@ -1,24 +1,24 @@
 
 window.addEventListener('load',function(){
 
-const divs = document.querySelectorAll('.glider');
+	const divs = document.querySelectorAll('.glider');
 
-// Itera sobre cada div
-divs.forEach((div, index) => {
-    // Adiciona um número à classe existente ou adiciona uma nova classe com o número
-    div.classList.add(`glider-${index + 1}`);
+	// Itera sobre cada div
+	divs.forEach((div, index) => {
+	    // Adiciona um número à classe existente ou adiciona uma nova classe com o número
+	    div.classList.add(`glider-${index + 1}`);
 
-    // Inicializa o Glider.js para cada div
-    new Glider(div, {
-        slidesToShow: 'auto',
-	    slidesToScroll: 1,
-	    itemWidth: 800,
-	    draggable: true,
-	    scrollLock: false,
-	    dots: false,
-	    rewind: true,
-        arrows: false,
-    });
-});
+	    // Inicializa o Glider.js para cada div
+	    new Glider(div, {
+	        slidesToShow: 'auto',
+		    slidesToScroll: 1,
+		    itemWidth: 800,
+		    draggable: true,
+		    scrollLock: false,
+		    dots: false,
+		    rewind: true,
+	        arrows: false,
+	    });
+	});
 
 });
