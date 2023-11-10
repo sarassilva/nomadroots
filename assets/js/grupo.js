1707b07e4d274ff50ce914df138a41e2ad45f02d
Sara <sarassilvadw@gmail.com>
2023-11-10 15:24:32 -0300

    Update grupo.js

--- assets/js/grupo.js
+++ assets/js/grupo.js
@@ -3,21 +3,13 @@ window.addEventListener('load',function(){
 
 	for (var i = 0; i < sliders.length; i++) {
 	  var glide = new Glide(sliders[i], {
+	    gap: 15,
 	    slidesToShow: 'auto',
-	    slidesToScroll: 1,
-	    draggable: false,
-	    scrollLock: false,
-	    dots: false,
-	    rewind: true,
-	    arrows: {
-	        prev: '.glider-prev',
-	        next: '.glider-next'
-	    }
 	  });
 	  
 	  glide.mount();
 	}
-});
+}
 
 $(function(){
 $('.popup').on('click', function(){
