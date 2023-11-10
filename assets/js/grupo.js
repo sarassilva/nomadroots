6e078dc29bcabcc9e9c4a043dc2ef67b0d556be8
Sara <sarassilvadw@gmail.com>
2023-11-10 16:50:51 -0300

    Update grupo.js

--- assets/js/grupo.js
+++ assets/js/grupo.js
@@ -7,6 +7,17 @@ window.addEventListener('load', function () {
         // Adiciona um número à classe existente ou adiciona uma nova classe com o número
         div.classList.add(`glider-${index + 1}`);
 
+        // Obtém os seletores específicos para as setas de navegação desta div
+        const prevArrowSelector = `.glider-${index + 1} .glider-prev`;
+        const nextArrowSelector = `.glider-${index + 1} .glider-next`;
+
+        // Adiciona um número à classe das setas
+        const prevArrow = div.querySelector('.glider-prev');
+        const nextArrow = div.querySelector('.glider-next');
+
+        prevArrow.classList.add(`seta-${index + 1}`);
+        nextArrow.classList.add(`seta-${index + 1}`);
+
         // Inicializa o Glider.js para cada div
         const glider = new Glider(div, {
             slidesToShow: 'auto',
@@ -16,11 +27,11 @@ window.addEventListener('load', function () {
             scrollLock: false,
             dots: false,
             rewind: true,
+            arrows: {
+                prev: prevArrowSelector,
+                next: nextArrowSelector
+            }
         });
-
-        // Obtém as setas de navegação específicas para este carrossel
-        const prevArrow = div.querySelector('.glider-prev');
-        const nextArrow = div.querySelector('.glider-next');
 
         // Adiciona um ouvinte de evento para as setas de navegação
         if (prevArrow) {
