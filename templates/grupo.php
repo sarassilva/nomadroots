e119fe66e63826784f25a9fd3ce50ab72b56cd50
Sara <sarassilvadw@gmail.com>
2023-11-10 17:28:09 -0300

    popup

--- templates/grupo.php
+++ templates/grupo.php
@@ -72,6 +72,10 @@
 					        	<button onclick="popup()" class="btn outline popup">Ver roteiro completo</button>
 					        	<a href="<?php the_sub_field('comprar_viagem-se') ?>" title="" class="btn">Comprar viagem</a>
 					        </div>
+
+					        <div class="roteiroPopup">
+					        	<?php the_field('roteiro'); ?>
+					        </div>
 		            	</div>
 		            </li>
 		
