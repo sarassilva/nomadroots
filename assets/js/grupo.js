dbef5929e2bd3ac0d1c7b5801697713546724f48
Sara <sarassilvadw@gmail.com>
2023-11-10 15:34:00 -0300

    Update grupo.js

--- assets/js/grupo.js
+++ assets/js/grupo.js
@@ -2,7 +2,6 @@ window.addEventListener('load',function(){
 	new Glider(document.querySelector('.glider'), {
 	    slidesToShow: 'auto',
 	    slidesToScroll: 1,
-	    itemWidth: 400,
 	    draggable: false,
 	    scrollLock: false,
 	    dots: false,
