cb1533db229e964f008c2e0d1b8001b012f0551c
Sara <sarassilvadw@gmail.com>
2023-11-10 16:02:25 -0300

    Update grupo.js

--- assets/js/grupo.js
+++ assets/js/grupo.js
@@ -1,16 +1,14 @@
- window.addEventListener('load', function () {
-    // Selecione todas as divs com a classe "glider"
-    const gliders = document.querySelectorAll('.glider');
+// Obtém todas as divs na página
+const divs = document.querySelectorAll('.glider');
 
-    for (let i = 0; i < gliders.length; i++) {
-    	const glider = gliders[i];
-        new Glider(glider, {
-            slidesToShow: 'auto',
-            slidesToScroll: 1,
-            draggable: true,
-            dots: false,
-            rewind: true,
-            arrows: false,
-        });
-    });
+// Itera sobre cada div
+divs.forEach((div, index) => {
+    // Verifica se a div já possui uma classe
+    if (div.classList.length > 0) {
+        // Adiciona um número à classe existente
+        div.classList.add(`${div.classList[0]}-${index + 1}`);
+    } else {
+        // Se não tiver classe, adiciona uma classe com o número
+        div.classList.add(`minha-div-${index + 1}`);
+    }
 });
