c3d59e87a0fee59d1aefd9dbe2d54c4a55984fbd
Sara <sarassilvadw@gmail.com>
2023-11-10 19:57:26 -0300

    Update grupo.php

--- templates/grupo.php
+++ templates/grupo.php
@@ -118,15 +118,15 @@
 			    		<div class="img"></div>
 			    		<div class="content">
 			    			<span class="status">
-			    				<?php $terms = get_the_terms( $post->ID , 'grupo-nomad' );
-							if ( is_array( $terms ) && ! is_wp_error( $terms ) ) {
-							    foreach ($terms as $term) {
-							        $term_link = get_term_link($term, 'grupo-nomad');
-							        if (is_wp_error($term_link))
-							            continue;
-							        echo '. $term->name . ';
-							    }
-							}
+			    				<?php  $taxonomy_terms = get_the_terms( get_the_ID(), 'status-da-viagem');
+			    				if ( $taxonomy_terms && ! is_wp_error( $taxonomy_terms ) ) {
+						            // Exibe os termos
+						            echo '<ul>';
+						            foreach ( $taxonomy_terms as $term ) {
+						                echo '<li>' . esc_html( $term->name ) . '</li>';
+						            }
+						            echo '</ul>';
+						        }
 							?>
 							</span>
 			    			<h5><?php the_title(); ?></h5>
