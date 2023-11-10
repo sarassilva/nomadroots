6cb276df3a220fcdfd2c747815534e5d4bdc3aae
Sara <sarassilvadw@gmail.com>
2023-11-10 09:45:55 -0300

    Grupos

--- templates/grupo.php
+++ templates/grupo.php
@@ -18,6 +18,33 @@
 			<?php the_field('descricao') ?>      		
 		</div>
 	</section>
+
+	<section class="nextTrips">
+		<div class="container">
+			<h2>Próximas viagens</h2>
+
+			<ul>
+				<?php
+			    $new_loop = new WP_Query( array(
+			    'post_type' => 'roteiro',
+			    'posts_per_page' => 2,
+			    'tax_query' => array(
+		            array(
+		                'taxonomy' => 'destino',
+		                'field' => 'slug',
+		                'terms' => array( 'roteiro-1' ),
+		            ),
+		        ),
+			    ) ); ?>
+
+			    <?php if ( $new_loop->have_posts() ) : ?>
+			    <?php while ( $new_loop->have_posts() ) : $new_loop->the_post(); ?>
+			    	<li>
+			    		<h4><?php the_title(); ?></h4>
+			    	</li>
+			</ul>
+		</div>
+	</section>
 </main>
 
 <?php get_footer(); ?>
