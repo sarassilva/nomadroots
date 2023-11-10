b04e9b28470637869cb632797eb2a5de74802a04
Sara <sarassilvadw@gmail.com>
2023-11-10 16:07:34 -0300

    Update grupo.js

--- assets/js/grupo.js
+++ assets/js/grupo.js
@@ -1,14 +1,27 @@
-// Obtém todas as divs na página
+
+window.addEventListener('load',function(){
+
+	// Obtém todas as divs com a classe '.glider' na página
 const divs = document.querySelectorAll('.glider');
 
 // Itera sobre cada div
 divs.forEach((div, index) => {
-    // Verifica se a div já possui uma classe
-    if (div.classList.length > 0) {
-        // Adiciona um número à classe existente
-        div.classList.add(`${div.classList[0]}-${index + 1}`);
-    } else {
-        // Se não tiver classe, adiciona uma classe com o número
-        div.classList.add(`minha-div-${index + 1}`);
-    }
+    // Adiciona um número à classe existente ou adiciona uma nova classe com o número
+    div.classList.add(`glider-${index + 1}`);
+
+    // Inicializa o Glider.js para cada div
+    new Glider(div, {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        scrollLock: false,
+        draggable: true,
+        rewind: true,
+        arrows: {
+            prev: `.glider-prev.whr-${index + 1}`,
+            next: `.glider-next.whr-${index + 1}`
+        },
+    });
+});
+
+
 });
