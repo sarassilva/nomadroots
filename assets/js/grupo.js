5837ff271d4901057ea8620de9b277eb5d3168ce
Sara <sarassilvadw@gmail.com>
2023-11-10 17:15:50 -0300

    Update grupo.js

--- assets/js/grupo.js
+++ assets/js/grupo.js
@@ -1,24 +1,12 @@
  window.addEventListener('load', function () {
             [].forEach.call(document.querySelectorAll('.glider'), function (ele) {
-                ele.addEventListener('glider-slide-visible', function (event) {
-                    var glider
-                        = Glider(this); console.log('Slide Visible %s', event.detail.slide)
-                });
-                ele.addEventListener('glider-slide-hidden', function (event) {
-                    console.log('Slide Hidden %s', event.detail.slide)
-                });
-                ele.addEventListener('glider-refresh', function (event) {
-                    console.log('Refresh')
-                });
-                ele.addEventListener('glider-loaded', function
-                    (event) {
-                        console.log('Loaded')
-                });
+
                 new Glider(ele, {
                     slidesToShow: 1,
                     slidesToScroll: 1,
                     scrollLock: true,
                     draggable: true,
+                    rewind: true,
                     dots: ele.querySelector('.dots'),
                     arrows: {
                         prev: ele.parentNode.querySelector('.glider-prev'),
