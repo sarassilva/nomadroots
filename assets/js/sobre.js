5ca60cf5e8ddcbe854a63b17371af5ad35ad3e54
Sara Silva <sarassilvadw@gmail.com>
2023-11-13 15:09:44 -0300

    Update sobre.js

--- assets/js/sobre.js
+++ assets/js/sobre.js
@@ -37,16 +37,13 @@ window.addEventListener('load',function(){
 	    arrows: false,
 	    responsive: [
 	   	{
-	      // screens greater than >= 1024px
 	      breakpoint: 700,
 	      settings: {
 	        slidesToShow: 2.5,
 	        slidesToScroll: 1.5,
 	        itemWidth: 150,
 	        duration: 0.25
-	    },
-		{
-	      // screens greater than >= 1024px
+	    },{
 	      breakpoint: 1024,
 	      settings: {
 	        slidesToShow: 3.5,
@@ -59,3 +56,45 @@ window.addEventListener('load',function(){
 	});
 });
 
+window.addEventListener('load',function(){
+	new Glider(document.querySelector('.tCarousel'), {
+	    slidesToShow: 1,
+	    slidesToScroll: 1,
+	    itemWidth: 400,
+	    draggable: true,
+	    scrollLock: false,
+	    dots: false,
+	    rewind: true,
+	    arrows: {
+	        prev: '.glider-prev.tN',
+	        next: '.glider-next.tN'
+	    },
+	    responsive: [
+		{
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 2.5,
+	        slidesToScroll: 1.5,
+	        itemWidth: 150,
+	        duration: 0.25
+	      }
+	    }, {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 3.5,
+	        slidesToScroll: 1.5,
+	        itemWidth: 150,
+	        duration: 0.25
+	      }
+	    }, {
+	      breakpoint: 1700,
+	      settings: {
+	        slidesToShow: 4.5,
+	        slidesToScroll: 1.5,
+	        itemWidth: 150,
+	        duration: 0.25
+	      }
+	    }
+	  ]
+	});
+});
