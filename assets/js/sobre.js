f5ac5b9df0e8922070d556f5ceeaaa2c01f25c68
Sara Silva <sarassilvadw@gmail.com>
2023-11-13 15:15:41 -0300

    Update sobre.js

--- assets/js/sobre.js
+++ assets/js/sobre.js
@@ -10,7 +10,19 @@ window.addEventListener('load',function(){
 	    arrows: {
 	        prev: '.glider-prev.nc',
 	        next: '.glider-next.nc'
+	    },
+	    responsive: [
+		{
+	      // screens greater than >= 1024px
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 3,
+	        slidesToScroll: 1,
+	        itemWidth: 150,
+	        duration: 0.25
+	      }
 	    }
+	  ]
 	});
 });
 
@@ -23,6 +35,25 @@ window.addEventListener('load',function(){
 	    dots: false,
 	    rewind: true,
 	    arrows: false,
+	    responsive: [
+	   	{
+	      breakpoint: 700,
+	      settings: {
+	        slidesToShow: 2.5,
+	        slidesToScroll: 1.5,
+	        itemWidth: 150,
+	        duration: 0.25,
+	    	}
+		}, {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 3.5,
+	        slidesToScroll: 1.5,
+	        itemWidth: 150,
+	        duration: 0.25,
+	      }
+	    }
+	  ]
 	});
 });
 
@@ -38,6 +69,33 @@ window.addEventListener('load',function(){
 	    arrows: {
 	        prev: '.glider-prev.tN',
 	        next: '.glider-next.tN'
+	    },
+	    responsive: [
+		{
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 2.5,
+	        slidesToScroll: 1.5,
+	        itemWidth: 150,
+	        duration: 0.25
+	      }
+	    }, {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 3.5,
+	        slidesToScroll: 1.5,
+	        itemWidth: 150,
+	        duration: 0.25
+	      }
+	    }, {
+	      breakpoint: 1700,
+	      settings: {
+	        slidesToShow: 4.5,
+	        slidesToScroll: 1.5,
+	        itemWidth: 150,
+	        duration: 0.25
+	      }
 	    }
+	  ]
 	});
 });
