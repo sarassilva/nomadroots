2693a19e3af8813b7dfeb5bb792d650b677aec73
Sara Silva <sarassilvadw@gmail.com>
2023-11-28 11:27:13 -0300

    grupo single

--- single-grupo-nomad.php
+++ single-grupo-nomad.php
@@ -1,15 +1,35 @@
 <?php get_header(); ?>
 
+    <main class="singleGroup">
+        <section class="main-banner" style="background-image: url('<?php the_field('imagem_destaque') ?>')">
+            <div class="content">
+                <h1><?php the_title(); ?></h1>
+                <div class="data"><?php the_field('data') ?></div>
+                <a href="<?php the_field('comprar_viagem') ?>" title="" class="btn">Compre agora</a>
+            </div>
+        </section>
 
-        <main class="singleGroup">
-            <section class="main-banner" style="background-image: url('<?php the_field('imagem_destaque') ?>')">
-                <div class="content">
-                    <h1><?php the_title(); ?></h1>
-                    <div class="data"><?php the_field('data') ?></div>
-                    <a href="<?php the_field('comprar_viagem') ?>" title="" class="btn">Comprar viagem</a>
+        <section class="hold">
+            <div class="container">
+            <?php if( have_rows('caixa_principal') ):
+		        while( have_rows('caixa_principal') ) : the_row();
+		        ?>
+                <div class="esquerda">
+                    <?php the_sub_field('esquerda') ?>
                 </div>
-            </section>
-        </main>
+                <div class="direita">
+                    <?php the_sub_field('direita') ?>
+                </div>
+            <?php endwhile; else : endif; ?>
+            </div>
+        </section>
+
+        <section class="description">
+            <div class="container">
+            <?php the_field('descricao') ?>
+            </div>
+        </section>
+    </main>
 
 
  
