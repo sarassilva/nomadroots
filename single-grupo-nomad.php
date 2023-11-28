9c6bad29c6bdc11c25cb2a1dc2f623a5c131170c
Sara Silva <sarassilvadw@gmail.com>
2023-11-28 11:04:14 -0300

    Updates single

--- single-grupo-nomad.php
+++ single-grupo-nomad.php
@@ -5,6 +5,7 @@
             <section class="main-banner" style="background-image: url('<?php the_field('imagem_destaque') ?>')">
                 <div class="content">
                     <h1><?php the_title(); ?></h1>
+                    <div class="data"><?php the_field('data') ?></div>
                     <a href="<?php the_field('comprar_viagem') ?>" title="" class="btn">Comprar viagem</a>
                 </div>
             </section>
