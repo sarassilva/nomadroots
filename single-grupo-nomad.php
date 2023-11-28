93e37547f3c5ecde6c4998f216e4c5f4a05eaf6a
Sara Silva <sarassilvadw@gmail.com>
2023-11-28 09:58:15 -0300

    Update single-grupo-nomad.php

--- single-grupo-nomad.php
+++ single-grupo-nomad.php
@@ -1,12 +1,17 @@
 <?php get_header(); ?>
 
-<main class="singleGroup">
-    <section class="main-banner" style="background-image: url('<?php the_field('imagem_destacada') ?>')">
-		<div class="content">
-			<h1><?php the_field('titulo_h1') ?></h1>
-			<a href="<?php the_sub_field('comprar_viagem') ?>" title="" class="btn">Comprar viagem</a>
-		</div>
-	</section>
-</main>
+    <?php while ( have_posts() ) : the_post(); ?>
 
+        <main class="singleGroup">
+            <section class="main-banner" style="background-image: url('<?php the_field('imagem_destacada') ?>')">
+                <div class="content">
+                    <h1><?php the_field('titulo_h1') ?></h1>
+                    <a href="<?php the_sub_field('comprar_viagem') ?>" title="" class="btn">Comprar viagem</a>
+                </div>
+            </section>
+        </main>
+
+    <?php endwhile;  ?>
+
+ 
 <?php get_footer(); ?>
