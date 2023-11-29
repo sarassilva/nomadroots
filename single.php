2bf3c4cd1a1118d31be2c2bda3a415aff06a744f
Sara Silva <sarassilvadw@gmail.com>
2023-11-28 22:32:01 -0300

    Update single.php

--- single.php
+++ single.php
@@ -212,6 +212,15 @@ global $product;
 
 ?>
 <?php echo $product->get_price_html(); ?>
+
+<table class="woocommerce-product-attributes shop_attributes">
+	<?php foreach ( $product_attributes as $product_attribute_key => $product_attribute ) : ?>
+		<tr class="woocommerce-product-attributes-item woocommerce-product-attributes-item--<?php echo esc_attr( $product_attribute_key ); ?>">
+			<th class="woocommerce-product-attributes-item__label"><?php echo wp_kses_post( $product_attribute['label'] ); ?></th>
+			<td class="woocommerce-product-attributes-item__value"><?php echo wp_kses_post( $product_attribute['value'] ); ?></td>
+		</tr>
+	<?php endforeach; ?>
+</table>
 <a href="<?php get_permalink( $post->ID ); ?>">Add to cart</a>
     </section>
 
