511fc9d365287b998c20fd2f288a3add68f9c915
Sara Silva <sarassilvadw@gmail.com>
2023-11-29 09:19:12 -0300

    Update single.php

--- single.php
+++ single.php
@@ -211,14 +211,10 @@
 		<?php the_post(); ?>
 		
 		<div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
-			<?php do_action( 'woocommerce_before_single_product_summary' ); ?>
 			<div class="summary entry-summary">				
 				<?php do_action( 'woocommerce_single_product_summary' ); ?>
 			</div>	
-			<?php do_action( 'woocommerce_after_single_product_summary' );	?>
-			<?php do_action( 'woocommerce_after_single_product' ); ?>
 			<?php endwhile;  ?>
-			<?php do_action( 'woocommerce_after_main_content' );?>
     </section>
 
     </main>
