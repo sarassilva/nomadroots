51eec0b71109f54c6bfd0709731bac25e52f24e7
Sara Silva <sarassilvadw@gmail.com>
2023-12-05 20:53:25 -0300

    buy section

--- functions.php
+++ functions.php
@@ -73,6 +73,63 @@ register_sidebar( array(
 //hooks
 add_action( 'woocommerce_before_add_to_cart_quantity', 'woosuite_echo_qty_front_add_cart' );
 function woosuite_echo_qty_front_add_cart() {
-    echo '<div class="qty-label">NNúmero de viajantes: </div>';   
+    echo '<div class="label">Número de viajantes</div>';   
 }
+
+function woocommerce_quantity_input( $args = array(), $product = null, $echo = true ) {
+  
+    if ( is_null( $product ) ) {
+       $product = $GLOBALS['product'];
+    }
+  
+    $defaults = array(
+       'input_id' => uniqid( 'quantity_' ),
+       'input_name' => 'quantity',
+       'input_value' => '1',
+       'classes' => apply_filters( 'woocommerce_quantity_input_classes', array( 'input-text', 'qty', 'text' ), $product ),
+       'max_value' => apply_filters( 'woocommerce_quantity_input_max', -1, $product ),
+       'min_value' => apply_filters( 'woocommerce_quantity_input_min', 0, $product ),
+       'step' => apply_filters( 'woocommerce_quantity_input_step', 1, $product ),
+       'pattern' => apply_filters( 'woocommerce_quantity_input_pattern', has_filter( 'woocommerce_stock_amount', 'intval' ) ? '[0-9]*' : '' ),
+       'inputmode' => apply_filters( 'woocommerce_quantity_input_inputmode', has_filter( 'woocommerce_stock_amount', 'intval' ) ? 'numeric' : '' ),
+       'product_name' => $product ? $product->get_title() : '',
+    );
+  
+    $args = apply_filters( 'woocommerce_quantity_input_args', wp_parse_args( $args, $defaults ), $product );
+   
+    // Apply sanity to min/max args - min cannot be lower than 0.
+    $args['min_value'] = max( $args['min_value'], 0 );
+    // Note: change 20 to whatever you like
+    $args['max_value'] = 0 < $args['max_value'] ? $args['max_value'] : 20;
+  
+    // Max cannot be lower than min if defined.
+    if ( '' !== $args['max_value'] && $args['max_value'] < $args['min_value'] ) {
+       $args['max_value'] = $args['min_value'];
+    }
+   
+    $options = '';
+     
+    for ( $count = $args['min_value']; $count <= $args['max_value']; $count = $count + $args['step'] ) {
+  
+       // Cart item quantity defined?
+       if ( '' !== $args['input_value'] && $args['input_value'] >= 1 && $count == $args['input_value'] ) {
+         $selected = 'selected';      
+       } else $selected = '';
+  
+       $options .= '<option value="' . $count . '"' . $selected . '>' . $count . '</option>';
+  
+    }
+      
+    $string = '<div class="quantity"><span>Qty</span><select name="' . $args['input_name'] . '">' . $options . '</select></div>';
+  
+    if ( $echo ) {
+       echo $string;
+    } else {
+       return $string;
+    }
+   
+ }
+
+}
+
 ?>
