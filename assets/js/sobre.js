2ff9135d64254eb2842f5348e976e4274633b6a0
Sara Silva <sarassilvadw@gmail.com>
2023-12-07 14:58:26 -0300

    timecarousel

--- assets/js/sobre.js
+++ assets/js/sobre.js
@@ -78,7 +78,7 @@ window.addEventListener('load',function(){
 		{
 	      breakpoint: 600,
 	      settings: {
-	        slidesToShow: 2.5,
+	        slidesToShow: 1.5,
 	        slidesToScroll: 1.5,
 	        itemWidth: 150,
 	        duration: 0.25
@@ -86,7 +86,7 @@ window.addEventListener('load',function(){
 	    }, {
 	      breakpoint: 1024,
 	      settings: {
-	        slidesToShow: 3.5,
+	        slidesToShow: 2.5,
 	        slidesToScroll: 1.5,
 	        itemWidth: 150,
 	        duration: 0.25
