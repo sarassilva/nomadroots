f05e4d7b5b27e96740327d8fb36566de018d636e
Sara Silva <sarassilvadw@gmail.com>
2023-12-12 16:19:50 -0300

    Update related.php

--- woocommerce/single-product/related.php
+++ woocommerce/single-product/related.php
@@ -28,7 +28,7 @@ if ( $related_products ) : ?>
 
 		if ( $heading ) :
 			?>
-			<h2><?php echo esc_html( $heading ); ?></h2>
+			<h3><?php echo esc_html( $heading ); ?></h3>
 		<?php endif; ?>
 		
 		<?php woocommerce_product_loop_start(); ?>
