9bbb664c560db78b41cffc3bba17a251258e1891
Sara Silva <sarassilvadw@gmail.com>
2023-12-12 11:22:03 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -131,4 +131,21 @@ function woocommerce_quantity_input($args = array(), $product = null, $echo = tr
   </div>';
 }
 
+//replace prices range with active variation price
+add_action( 'woocommerce_variable_add_to_cart', 'bbloomer_update_price_with_variation_price' );
+  
+function bbloomer_update_price_with_variation_price() {
+   global $product;
+   $price = $product->get_price_html();
+   wc_enqueue_js( "     
+      $(document).on('found_variation', 'form.cart', function( event, variation ) {   
+         if(variation.price_html) $('.summary > p.price').html(variation.price_html);
+         $('.woocommerce-variation-price').hide();
+      });
+      $(document).on('hide_variation', 'form.cart', function( event, variation ) {   
+         $('.summary > p.price').html('" . $price . "');
+      });
+   " );
+}
+
 ?>
