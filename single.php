ff754e07d66f628ef5911273f688fbb66faafa37
Sara Silva <sarassilvadw@gmail.com>
2023-12-13 14:21:25 -0300

    Update single.php

--- single.php
+++ single.php
@@ -336,7 +336,6 @@
 <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/glider.css" />
 <script src="<?php echo get_template_directory_uri(); ?>/assets/js/glider.js"></script>
 
-<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/assets/js/single.js"></script>
  
 <?php get_footer(); ?>
