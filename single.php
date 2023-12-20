6dba758d8540845be6d21d7cf5045334fe320d68
Sara Silva <sarassilvadw@gmail.com>
2023-12-20 17:03:19 -0300

    Update single.php

--- single.php
+++ single.php
@@ -299,11 +299,12 @@
             <div class="black">
                 <div class="container">
                         <h2><?php the_field('titulo_compra') ?></h2>
+                        <?php the_field('informacoes') ?>  
                             <p class="preco <?php echo esc_attr( apply_filters( 'woocommerce_product_price_class', 'price' ) ); ?>"><?php echo $product->get_price_html(); ?></p>
                         <div class="relative">                       
                             <?php do_action( 'woocommerce_single_product_summary' ); ?>  
                         </div>
-                        <?php the_field('informacoes') ?>                 
+                                      
                 </div>
             </div>
         </div>
