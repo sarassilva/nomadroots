80b65652858632e12dd1b250dbfaee93e341fa57
Sara Silva <sarassilvadw@gmail.com>
2024-01-08 16:29:48 -0300

    Update single.php

--- single.php
+++ single.php
@@ -194,7 +194,7 @@
                         <?php if( have_rows('roteiro') ): ?>
                             <?php while( have_rows('roteiro') ): the_row(); ?>                                
                                 <div class="content">
-                                <div class="data"><span><?php the_sub_field('dias') ?></span></div>
+                                <div class="data"><span><?php the_sub_field('dia') ?></span></div>
                                     <div class="imagem">
                                         <img src="<?php the_sub_field('imagem') ?>" />
                                     </div>
